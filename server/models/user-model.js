767b7fcf58859dbad66cc1bb0425131b96c81c48
Admin <anton.puzderi@gmail.com>
2025-11-12 20:53:25 +0300

    v2

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,10 +1,10 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const UserSchema  = new Schema({
-    email: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-    isActive: {type: Boolean, default: false},
-    activationLink: {type: String},
-})
+const UserSchema = new Schema({
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    isActive: { type: Boolean, default: false },
+    activationLink: { type: String },
+});
 
-model.exports = model('User', UserSchema)
+module.exports = model('User', UserSchema);
