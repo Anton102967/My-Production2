ca27d9526c0f556e4589f2faef66d76f17ea7f4e
Admin <anton.puzderi@gmail.com>
2025-11-12 19:58:43 +0300

    v1

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -14,6 +14,7 @@ class UserService {
         const activationLink = uuid.v4();
         const user = await UserModel.create({email, password: hashPassword, activationLink});
         await mailService.sendActivationMail(email, activationLink);
+        const tokens = tokenService.generateTokens();
     }
 }
 
