23722aac5e612bd85d6cb13dbe9d505686caba52
Admin <anton.puzderi@gmail.com>
2025-11-13 17:08:29 +0300

    use Postman v3

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,22 +3,12 @@ const userService = require('../service/user-service')
 class UserController {
     async registration(req, res, next) {
         try {
-             let body = req.body;
-
-            if (typeof body === 'string') {
-                try {
-                    body = JSON.parse(body);
-                } catch (parseError) {
-                    return res.status(400).json({ message: 'Invalid request body. Expected JSON.' });
-                }
-            }
-            const { email, password } = body || {};
+            const {email, password} = req.body;
             const userData = await userService.registration(email, password);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
             return res.json(userData);
         } catch (e) {
             console.log(e)
-            return res.status(400).json({ message: e.message });
         }
     }
 
